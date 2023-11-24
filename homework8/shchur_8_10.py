5cb8a13563d5bc1cf6460e8488c586222d4aafda
Oleksandr Shchur <131793496+shchursasha1@users.noreply.github.com>
2023-11-24 11:05:49 +0200

    implementation of permutations with a recursive function

--- homework8/shchur_8_10.py
+++ homework8/shchur_8_10.py
@@ -1,12 +1,15 @@
-import itertools
+def permutations(elements):
+    if len(elements) <= 1:
+        yield elements
+        return
+
+    for perm in permutations(elements[1:]):
+        for i in range(len(elements)):
+            yield perm[:i] + elements[0:1] + perm[i:]
+
 
 n = int(input())
-my_list = []
+my_list = [str(j) for j in range(1, n + 1)]
 
-for i in range(1, n + 1):
-    my_list.append(i)
-
-permutations = list(itertools.permutations(my_list))
-
-for p in permutations:
-    print(*p)
+for perm in sorted(permutations(my_list)):
+    print(' '.join(perm))
